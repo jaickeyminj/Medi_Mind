cb244ee0cbb736e8308ab5a5d39bb50d9051c95d
jaickeyminj <jaickey09@gmail.com>
2024-03-14 17:25:52 +0530

    Testing Changes

--- app/src/main/java/com/jaickey09/medimind/MainActivity.java
+++ app/src/main/java/com/jaickey09/medimind/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        System.out.println();
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
